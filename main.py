330eeec20ace0cf710a74c6575a9cc2bbf84de52
Bezbakri <bezbakri@gmail.com>
2023-02-24 22:33:36 -0600

    User interface possible now

--- main.py
+++ main.py
@@ -21,4 +21,17 @@ async def finally_work_pls(ctx):
     await ctx.channel.send(f"pong :ping_pong:\nMy latency is **{round(bot.latency*1000)} ms**")
 
 
+
+@bot.command()
+async def GetCarListing(ctx):
+    def check_author(m):
+        if m.author == ctx.author and m.channel == ctx.channel:
+            return True
+        return False
+    await ctx.send("What is your name?")
+    name = await bot.wait_for("message", check=check_author)
+    await ctx.send("What is your favorite color?")
+    color = await bot.wait_for("message", check=check_author)
+    await ctx.send(f"Hi {name.content}, who likes {color.content}")
+
 bot.run(DISCORD_TOKEN)
